9b046c8cbd2c10240f7dd029b9c3bb1cbea47227
Pierre Gaxatte <pierre.gaxatte@corp.ovh.com>
2020-04-30 09:15:58 +0200

    Fixed choices for configuration

--- ironic/conf/ovhapi.py
+++ ironic/conf/ovhapi.py
@@ -15,12 +15,19 @@
 from oslo_config import cfg
 
 from ironic.common.i18n import _
-from ironic.drivers.modules.ovhapi.ovh_base import ENDPOINTS
 
 opts = [
     cfg.StrOpt('endpoint',
                default='ovh-eu',
-               choices=ENDPOINTS.items(),
+               choices=[
+                   ('ovh-eu', 'https://eu.api.ovh.com/1.0'),
+                   ('ovh-us', 'https://api.us.ovhcloud.com/1.0'),
+                   ('ovh-ca', 'https://ca.api.ovh.com/1.0'),
+                   ('kimsufi-eu', 'https://eu.api.kimsufi.com/1.0'),
+                   ('kimsufi-ca', 'https://ca.api.kimsufi.com/1.0'),
+                   ('soyoustart-eu', 'https://eu.api.soyoustart.com/1.0'),
+                   ('soyoustart-ca', 'https://ca.api.soyoustart.com/1.0'),
+               ],
                help=_('Endpoint of the OVH API')),
     cfg.StrOpt('application_key',
                help=_('Application Key, see https://docs.ovh.com/gb/en/'
